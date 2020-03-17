e34312d8cef6f3fa19e682bea471219cf4149f20
Rabia Begum Dogan <r.begumdogan@gmail.com>
2020-03-17 20:04:59 +0000

    Add second step, handle hello and progress link click. Create new css file.

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ window.addEventListener('DOMContentLoaded', function(event){
     var helloLink = document.getElementById('hello-link');
     helloLink.addEventListener('click', function(linkEvent){
         linkEvent.preventDefault();
-        console.log('Hello world!');
+        this.classList.add('hide');
+        var intro = document.querySelector('.intro');
+        intro.classList.remove('hide');
+    });
+    var progressLink = document.getElementById('progress-link');
+    progressLink.addEventListener('click', function(linkEvent){
+        linkEvent.preventDefault();
+        this.classList.add('text');
+        var detail = document.querySelector('.detail');
+        detail.classList.remove('hide');
+        document.body.classList.add('font-family');
     })
 })
